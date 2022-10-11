81f889406e2a33026aea079cbf18ad478703fc24
Darimus <bruno.passos@outlook.com.br>
2022-10-11 18:57:24 -0300

    :smile: Refatorando e criando um componente responsavel pelo timer da aplicação

---
+++ src/components/timer.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+
+    <StopwatchComponent :timeInSeconds="timeInSeconds"></StopwatchComponent>
+
+    <button class="button" @click="start">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+
+    <button class="button" @click="pause">
+      <span class="icon">
+        <i class="fas fa-pause"></i>
+      </span>
+      <span>pause</span>
+    </button>
+
+    <button class="button" @click="stop">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import StopwatchComponent from './stopwatchComponent.vue'
+
+export default defineComponent({
+  name: "TimerComponent",
+  components: { StopwatchComponent },
+  data() {
+    return {
+      timeInSeconds: 0,
+      referenceForStop: 0
+    };
+  },
+  methods: {
+    start() {
+      this.referenceForStop = setInterval(() => {
+        this.timeInSeconds += 1;
+      }, 1000);
+    },
+    pause() {
+      clearInterval(this.referenceForStop);
+    },
+    stop() {
+      clearInterval(this.referenceForStop);
+    }
+  }
+})
+</script>
